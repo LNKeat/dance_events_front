690b0c8e8ed40a10e012d1fe36052819df799f5f
Laura <LNKEAT@gmail.com>
2023-01-30 15:27:46 -0700

    add layout and card to practice file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import BootstrapPract from './components/BootstrapPract';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+
 function App() {
   return (
-    <div className="App">
+    <Container fluid>
       <header className="App-header">
         <BootstrapPract />
+  
       </header>
-    </div>
+    </Container>
   );
 }
 
--- src/components/BootstrapPract.js
+++ src/components/BootstrapPract.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col, Card, CardBody, CardText, Button } from 'react-bootstrap';
+import Image from 'react-bootstrap/Image';
+
+
 
 const BootstrapPract = () => {
   return (
-    <div>
-        <h1>Header</h1>
-    </div>
+    <Container fluid className="boot-pract" style={{textAlign: 'center'}}>
+      <Row>
+        <Col>
+          <h1>Practice Styling Component</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <p>Something here</p>
+        </Col>
+      </Row>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" />
+          <Card.Body  style={{backgroundColor: '#e6e6e6', textAlign: 'start'}}>
+            <Card.Title>Dance Event Name</Card.Title>
+            <Card.Text>
+              Event Date: 
+            </Card.Text>
+            <Button className="btn" style={{backgroundColor: "#6c63ff", border:'none', textAlign: 'center'}}>Delete</Button>
+          </Card.Body>
+        </Card>
+      
+      <Row>
+        <Col style={{textAlign: 'start'}}>
+          <Image rounded src="./images/undraw_happy_music_g6wc.svg" width={200} height={200} />
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
