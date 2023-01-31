ab9dac57f60929b18e34c1b034b81dd548e2c940
Laura <LNKEAT@gmail.com>
2023-01-30 22:32:25 -0700

    adding state and controlled components on AddEventForm component

--- src/pages/AddEventForm.js
+++ src/pages/AddEventForm.js
@@ -1,11 +1,31 @@
-import { Col, Form, Row} from 'react-bootstrap';
+import { useState } from 'react';
+import { Col, Form, Row, Button } from 'react-bootstrap';
 
 function AddEventForm() {
+  //create state for form values
+  const [formValues, setFormValues] = useState({
+    "name": "",
+    "start": "",
+    "affordable": true,
+    "desc": "",
+    "location": ""
+  })
+
+  const handleChange = (e) => {
+    console.log(e.target)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(e)
+  }
+
   return (
-    <Form>
+    <Form onSubmit={onSubmit}>
       <Row>
         <Col>
-          <Form.Control placeholder="Event Name" />
+          <Form.Control placeholder="Event Name" name="name" value={formValues.name}
+          onChange={handleChange} />
         </Col>
         <Col>
           <Form.Control placeholder="Location" />
@@ -21,7 +41,13 @@ function AddEventForm() {
           <Form.Check type='checkbox' label="Affordable Event"/>
         </Col>
       </Row>
-      
+      <Row style={{textAlign: 'right'}}>
+        <Col>
+        <Button type="submit" style={{backgroundColor: '#6c63ff', border: 'none'}}>
+        Submit
+      </Button>
+        </Col>
+      </Row>
 
     </Form>
   );
